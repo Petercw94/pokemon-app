e0b3141fbc97b2cabedb29183b35590954498bd4
Peter <petercw94@gmail.com>
2022-10-15 16:51:40 -0600

    created a maven architechture along with basic code for requesting pokemon data from the API and printing it out in string form. Next step would be processing the response data with jackson library;

---
+++ src/main/java/com/peterwilliams/pokemon/App.java
@@ -0,0 +1,40 @@
+package com.peterwilliams.pokemon;
+
+import java.net.URLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // create an instance of the URL class
+        try {
+            URLConnection con = new URL("https://pokeapi.co/api/v2/pokemon/charizard").openConnection();
+            String charset = java.nio.charset.StandardCharsets.UTF_8.name();
+            con.setRequestProperty("Accept-Charset", charset);
+            InputStream response = con.getInputStream();
+            // con.close();
+            try (Scanner scanner = new Scanner(response)) {
+                String responseBody = scanner.useDelimiter("\\A").next();
+                System.out.println(responseBody);
+            } 
+        } catch(MalformedURLException ex) {
+            System.out.println("MalformedURLException: " + ex);
+        } catch(IOException ex) {
+            System.out.println("IOException: " + ex);
+        }
+        
+        
+        // create an instance of the HTTP connection class
+        // 1. The connection object is created by invoking the openConnection method on a URL.
+        // 2. The setup parameters and general request properties are manipulated.
+        // 3. The actual connection to the remote object is made, using the connect method.
+        // 4. The remote object becomes available. The header fields and the contents of the remote object can be accessed.
+        // URLConnection con = url.openConnection();
+    }
+}
