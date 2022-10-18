6ea0224a5ccd39b5ca847479382343853d8ce667
Peter <petercw94@gmail.com>
2022-10-17 19:32:34 -0600

    moved the pokemon class to its own file

--- src/main/java/com/peterwilliams/pokemon/App.java
+++ src/main/java/com/peterwilliams/pokemon/App.java
@@ -60,50 +60,5 @@ public class App
     }
 };
 
-class Pokemon 
-{
-    private String name;
-    private long height;
-    private long weight;
-    private long baseExperience;
-    private String frontSprite;
-    private String backSprite;
 
-    public Pokemon(JSONObject json)
-    {
-        this.name = (String) json.get("name");
-        this.height = (long) json.get("height");
-        this.weight = (long) json.get("weight");
-        this.baseExperience = (long) json.get("base_experience");
-        // accessing the sprite layer
-        JSONObject sprites = (JSONObject) json.get("sprites");
-        this.frontSprite = (String) sprites.get("front_default");
-        this.backSprite = (String) sprites.get("back_default");
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public long getHeight()
-    {
-        return height;
-    }
-
-    public long getWeight()
-    {
-        return weight;
-    }
-
-    public long getBaseExperience()
-    {
-        return baseExperience;
-    }
-
-    public String getFrontSprite()
-    {
-        return frontSprite;
-    }
-}
 
